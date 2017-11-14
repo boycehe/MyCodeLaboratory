88b5f6695963e3820be2558bbdbaed22b9b3a488
boyce <hepeilin1987@qq.com>
2017-11-14 23:24:56 +0800

    learn android contact

--- Android/app/src/main/java/com/example/heboyce/learnandroid/Chapter9/ContactContentProviderActivity.java
+++ Android/app/src/main/java/com/example/heboyce/learnandroid/Chapter9/ContactContentProviderActivity.java
@@ -1,9 +1,16 @@
 package com.example.heboyce.learnandroid.Chapter9;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebHistoryItem;
+import android.widget.Button;
 
 import com.example.heboyce.learnandroid.R;
+
+import java.util.ArrayList;
 
 public class ContactContentProviderActivity extends AppCompatActivity {
 
@@ -11,5 +18,46 @@ public class ContactContentProviderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_content_provider);
+
+        Button search = (Button)findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ArrayList<String> names = new ArrayList<>();
+                final ArrayList<ArrayList<String>> details = new ArrayList<>();
+                Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
+
+                while (cursor.moveToNext()){
+
+                    String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    names.add(name);
+                    Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = "+contactId,null,null);
+                    ArrayList<String> detail = new ArrayList<String>();
+                    while (phones.moveToNext()){
+                        String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        detail.add("电话号码:"+phoneNumber);
+                    }
+                    phones.close();
+                    Cursor emails = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,null,ContactsContract.CommonDataKinds.Email.CONTACT_ID+" = " + contactId,null,null);
+                    while (emails.moveToNext()){
+
+                        String emailAddress = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                        detail.add("邮件地址："+emailAddress);
+
+                    }
+
+                    emails.close();
+                    details.add(detail);
+
+                }
+
+                cursor.close();
+
+
+
+            }
+        });
+
     }
 }
