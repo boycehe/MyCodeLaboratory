3fb0d8b8c06a59e5bcab79e079d533c7539ac403
boyce <hepeilin1987@qq.com>
2017-12-20 17:01:08 +0800

    测试网络

--- iOS/ObjC/ObjCDemo/ObjCDemo/AppDelegate.m
+++ iOS/ObjC/ObjCDemo/ObjCDemo/AppDelegate.m
@@ -15,92 +15,7 @@
 #import "TestProgressView.h"
 #import "ViewController.h"
 #import "AutolayoutViewController.h"
-
-typedef NS_OPTIONS(NSUInteger, SDWebImageOptions) {
-    /**
-     * By default, when a URL fail to be downloaded, the URL is blacklisted so the library won't keep trying.
-     * This flag disable this blacklisting.
-     */
-    SDWebImageRetryFailed = 1 << 0,
-    
-    /**
-     * By default, image downloads are started during UI interactions, this flags disable this feature,
-     * leading to delayed download on UIScrollView deceleration for instance.
-     */
-    SDWebImageLowPriority = 1 << 1,
-    
-    /**
-     * This flag disables on-disk caching
-     */
-    SDWebImageCacheMemoryOnly = 1 << 2,
-    
-    /**
-     * This flag enables progressive download, the image is displayed progressively during download as a browser would do.
-     * By default, the image is only displayed once completely downloaded.
-     */
-    SDWebImageProgressiveDownload = 1 << 3,
-    
-    /**
-     * Even if the image is cached, respect the HTTP response cache control, and refresh the image from remote location if needed.
-     * The disk caching will be handled by NSURLCache instead of SDWebImage leading to slight performance degradation.
-     * This option helps deal with images changing behind the same request URL, e.g. Facebook graph api profile pics.
-     * If a cached image is refreshed, the completion block is called once with the cached image and again with the final image.
-     *
-     * Use this flag only if you can't make your URLs static with embedded cache busting parameter.
-     */
-    SDWebImageRefreshCached = 1 << 4,
-    
-    /**
-     * In iOS 4+, continue the download of the image if the app goes to background. This is achieved by asking the system for
-     * extra time in background to let the request finish. If the background task expires the operation will be cancelled.
-     */
-    SDWebImageContinueInBackground = 1 << 5,
-    
-    /**
-     * Handles cookies stored in NSHTTPCookieStore by setting
-     * NSMutableURLRequest.HTTPShouldHandleCookies = YES;
-     */
-    SDWebImageHandleCookies = 1 << 6,
-    
-    /**
-     * Enable to allow untrusted SSL certificates.
-     * Useful for testing purposes. Use with caution in production.
-     */
-    SDWebImageAllowInvalidSSLCertificates = 1 << 7,
-    
-    /**
-     * By default, images are loaded in the order in which they were queued. This flag moves them to
-     * the front of the queue.
-     */
-    SDWebImageHighPriority = 1 << 8,
-    
-    /**
-     * By default, placeholder images are loaded while the image is loading. This flag will delay the loading
-     * of the placeholder image until after the image has finished loading.
-     */
-    SDWebImageDelayPlaceholder = 1 << 9,
-    
-    /**
-     * We usually don't call transformDownloadedImage delegate method on animated images,
-     * as most transformation code would mangle it.
-     * Use this flag to transform them anyway.
-     */
-    SDWebImageTransformAnimatedImage = 1 << 10,
-    
-    /**
-     * By default, image is added to the imageView after download. But in some cases, we want to
-     * have the hand before setting the image (apply a filter or add it with cross-fade animation for instance)
-     * Use this flag if you want to manually set the image in the completion when success
-     */
-    SDWebImageAvoidAutoSetImage = 1 << 11,
-    
-    /**
-     * By default, images are decoded respecting their original size. On iOS, this flag will scale down the
-     * images to a size compatible with the constrained memory of devices.
-     * If `SDWebImageProgressiveDownload` flag is set the scale down is deactivated.
-     */
-    SDWebImageScaleDownLargeImages = 1 << 12
-};
+#import "PullRefreshDemoViewController.h"
 
 @interface AppDelegate ()
 
@@ -127,15 +42,11 @@ typedef NS_OPTIONS(NSUInteger, SDWebImageOptions) {
     iPhone 8 Plus {{0, 0}, {414, 736}}
     */
     
-    SDWebImageOptions type = 0;
-    
-    if (type == SDWebImageRetryFailed) {
-        NSLog(@"SDWebImageRetryFailed");
-    }
+  
   
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   
-    AutolayoutViewController *v = [[AutolayoutViewController alloc] init];
+    PullRefreshDemoViewController *v = [[PullRefreshDemoViewController alloc] init];
     self.window.rootViewController = v;
     
     [self.window makeKeyAndVisible];
--- iOS/ObjC/ObjCDemo/ObjCDemo/PullRefresh/PullRefreshDemoViewController.m
+++ iOS/ObjC/ObjCDemo/ObjCDemo/PullRefresh/PullRefreshDemoViewController.m
@@ -8,7 +8,8 @@
 
 #import "PullRefreshDemoViewController.h"
 
-@interface PullRefreshDemoViewController ()
+@interface PullRefreshDemoViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,22 +17,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate   = self;
+    
     // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 100;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *cellIdentifier = @"UITableView";
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"cell-%zd",indexPath.row];
+    return cell;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
